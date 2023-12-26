98fa66aef3bc3b7d1f2c74af302fe6e3cc4c0610
Thiago Marques <thiagomarquessilva99@gmail.com>
2023-12-26 17:49:50 -0300

    Implementação da lógica de criação do qr code

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,90 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import QRCode from 'qrcode'
+
 import './App.css'
+import { useState } from 'react'
 
 function App() {
-  const [count, setCount] = useState(0)
 
+  const [textDigited, setTextDigited] = useState("");
+  const [downloadPermitted, setDonwloadPermitted] = useState(false);
+
+  /*useEffect(() => {
+
+    QRCode.toCanvas(document.getElementById('canvas'), 'sample text', function (error) {
+      if (error) console.error(error)
+      console.log('success!');
+    })
+
+  }, [])*/
+
+  const clean = () => {
+
+    setDonwloadPermitted(false);
+    setTextDigited("");
+
+    const canvas = document.getElementById('canvas');
+    const context = canvas.getContext('2d');
+    context.clearRect(0, 0, canvas.width, canvas.height);
+
+  }
+
+  const donwloadQRCode = () => {
+
+    if(downloadPermitted){
+
+      const link = document.createElement('a');
+      link.download = 'qrcode.png';
+      link.href = document.getElementById('canvas').toDataURL();
+      link.click();
+
+    }else{
+
+      alert("Não foi possível fazer o download do QR Code.");
+    }
+  }
+
+  const generateQRCode = () => {
+
+    if(textDigited.length === 0){
+
+      alert("O texto não pode ser vazio!");
+    }else{
+
+      QRCode.toCanvas(document.getElementById('canvas'), textDigited, function (error) {
+
+        if (error){
+
+          alert(error);
+          setDonwloadPermitted(false);
+        }else{
+
+          setDonwloadPermitted(true);
+        }
+      })
+    }
+  }
+
+  const handleValue = (e) => {
+
+    setTextDigited(e.target.value);
+  }
+  
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className='container'>
+
+      <h1>Gerador de QR Code</h1>
+
+      <canvas id="canvas"></canvas>
+      
+      <input type="text" value={textDigited} className='text-input' onChange={handleValue} placeholder='Digite aqui um texto ou a URL de um site...'/>
+
+      <div className='container-buttons'>
+
+        <button onClick={generateQRCode}>Gerar</button>
+        <button onClick={clean}>Limpar</button>
+        <button onClick={donwloadQRCode}>Download</button>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    </div>
   )
 }
 
